f822d4914389824368bd5529b3dd33f111cda8d5
Arthur91S <arturs.stulpe@gmail.com>
2020-09-14 15:01:18 +0100

    Part 2 done

--- src/arthur/fizzbuzz/Main.java
+++ src/arthur/fizzbuzz/Main.java
@@ -3,30 +3,47 @@ package arthur.fizzbuzz;
 public class Main {
 
     public static void main(String[] args) {
+        String fezz = "";
+        Boolean reverse;
 
-        for (int i = 1; i < 100; i++){
+        for (int i = 1; i < 260; i++){
+        fezz = (i % 13 == 0 ? "Fezz" : "");
+        reverse = i % 17 == 0;
+
             if (i%3 == 0 && i%5 == 0) {
-                System.out.println(i+" FizzBuzz");
+                if(reverse){
+                    System.out.println(i+" Buzz"+fezz+"Fizz");
+                } else {
+                    System.out.println(i+" Fizz"+fezz+"Buzz");
+                }
             }
             else if (i % 3 == 0) {
                 if (i % 7 == 0) {
-                    System.out.println(i+ " FizzBang");
+                    if (reverse) {
+                        System.out.println(i+ " Bang" +fezz+ "Fizz");
+                    } else  {
+                        System.out.println(i+ " Fizz" +fezz+ "Bang");
+                    }
                 } else {
                     System.out.println(i+ " Fizz");
                 }
             } else if (i% 5 == 0) {
-                System.out.println(i+" Buzz");
+                if (reverse) {
+                    System.out.println(i+" Buzz"+ fezz);
+                } else {
+                    System.out.println(i+" " + fezz +"Buzz");
+                }
             } else if (i % 7 == 0){
                 System.out.println(i+" Bang");
             } else if (i % 11 == 0) {
-                System.out.println(i+" Bong");
-            } else if (i % 13 == 0) {
-                if (i % 3 ==0 || i % 5 ==0 ){
-                    System.out.println(i+ "");
+                if (reverse) {
+                    System.out.println(i+" Bong"+ fezz);
                 } else {
-                    System.out.println(i+ " Fezz");
+                    System.out.println(i+" "+ fezz +"Bong");
                 }
-            }else {
+            } else if (i % 13 == 0) {
+                System.out.println(i+ " Fezz");
+            } else {
                 System.out.println(i);
             }
         }
