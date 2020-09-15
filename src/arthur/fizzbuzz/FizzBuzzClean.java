0cb71c6b8d840e8941deda17c42064ab27c0bbb7
Arthur91S <arturs.stulpe@gmail.com>
2020-09-15 16:42:59 +0100

    Added multiples of 13 part

--- src/arthur/fizzbuzz/FizzBuzzClean.java
+++ src/arthur/fizzbuzz/FizzBuzzClean.java
@@ -4,7 +4,7 @@ public class FizzBuzzClean {
 
     public void fizzbuzz() {
 
-        for (int i = 1; i < 100; i++) {
+        for (int i = 1; i < 205; i++) {
 
             String text = "";
 
@@ -20,21 +20,24 @@ public class FizzBuzzClean {
                 text += "Bang";
             }
 
-            if (i % 11 == 0){
+            if (i % 11 == 0) {
                 text = "Bong";
             }
 
-            if (i % 13 == 0){
-                text = "Fezz";
+            if (i % 13 == 0 ) {
+                int indexOfB = text.indexOf("B");
+                if (indexOfB == -1){
+                    text = "Fezz" + text;
+                } else {
+                    text = text.substring(0, indexOfB) + "Fezz" + text.substring(indexOfB);
+                }
             }
 
-            if (!text.equals("")){
-                System.out.println(text);
-            } else {
-                System.out.println(i);
+            if (text.equals("")) {
+               text += Integer.toString(i);
             }
+
+            System.out.println(text);
         }
     }
-
-
 }
